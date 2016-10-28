661fa52038af9fda9074992eddda6799318bf515
Cody Chan <int64ago@gmail.com>
2016-10-28 09:02:30 +0800

    Add missed configure file

---
+++ example/moky.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+module.exports = {
+  /* Listen port for moky server, OPTIONAL */
+  localPort: 3000,
+  /* Asnyc api mock data, OPTIONAL */
+  asyncMockPath: path.join(__dirname, 'mock'),
+  /* Template mock data, OPTIONAL */
+  viewsMockPath: path.join(__dirname, 'tplMock'),
+  /* Root directory for template rendering, REQUIRED */
+  viewsPath: path.join(__dirname, 'views'),
+  /* Path of favicon.ico, OPTIONAL */
+  faviconPath: path.join(__dirname, 'public', 'favicon.ico'),
+  /* Static router, OPTIONAL but usually required */
+  publicPaths: {
+    '/css': path.join(__dirname, 'public', 'css'),
+    '/js': path.join(__dirname, 'public', 'js'),
+  },
+  /* Template engine settings, the same as koa-views, REQUIRED */
+  viewConfig: {
+    extension: 'html',
+    map: { html: 'nunjucks' },
+  },
+  /* Settings for proxy, OPTIONAL */
+  proxyMaps: {
+    dev: 'https://hacker-news.firebaseio.com',
+    local: 'http://localhost:8080',
+  },
+  /* Settings for template page routing, REQUIRED */
+  urlMaps: {
+    '/': 'index',
+    '/page': 'page/index',
+  }
+}
